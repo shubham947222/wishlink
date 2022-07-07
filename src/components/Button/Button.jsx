428e6b2fa87927f8ee73c422ed514b561acfcd08
shubham947222 <shubham947222@gmail.com>
2022-07-07 12:25:08 +0530

    minor change{link added} in button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./button.css";
 const Button = ({ text }) => {
-  return <button className="shopBtn">{text}</button>;
+  return (
+    <button className="shopBtn">
+      <p className="btntext">
+        <a href="https://home.wishlink.com/">{text}</a>
+      </p>
+    </button>
+  );
 };
 
 export default Button;
