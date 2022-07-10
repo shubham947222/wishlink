d8bde7a3962afba3b321a582096ffe04d0cab511
shubham947222 <shubham947222@gmail.com>
2022-07-10 17:16:07 +0530

    fix in banner video card

--- src/components/BannerVideoCard/BannerVideoCard.jsx
+++ src/components/BannerVideoCard/BannerVideoCard.jsx
@@ -34,51 +34,9 @@ function BannerVideoCard() {
           </div>
         </div>
       </Carousel.Item>
-      {/*  */}
+
       <Carousel.Item>
-        <div className="BannerVideoCard">
-          <div className="left">
-            <img className="videoCardImage" src={videoCardImage} alt="" />
-          </div>
-
-          <div className="text">
-            <h3 className="dressName">TRACE</h3>
-            <p className="description"> Long red slit dress</p>
-            <div className="shop-flex">
-              <p className="price">₹1600</p>
-              <p className="originalPrice">₹2000</p>
-              <p className="discount">(20% OFF)</p>
-            </div>
-          </div>
-
-          <div className="cardBtn">
-            <Button text="Shop" />
-          </div>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="BannerVideoCard">
-          <div className="left">
-            <img className="videoCardImage" src={videoCardImage} alt="" />
-          </div>
-
-          <div className="text">
-            <h3 className="dressName">TRACE</h3>
-            <p className="description"> Long red slit dress</p>
-            <div className="shop-flex">
-              <p className="price">₹1600</p>
-              <p className="originalPrice">₹2000</p>
-              <p className="discount">(20% OFF)</p>
-            </div>
-          </div>
-
-          <div className="cardBtn">
-            <Button text="Shop" />
-          </div>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="BannerVideoCard">
+        <div className="BannerVideoCard2 BannerVideoCard">
           <div className="left">
             <img className="videoCardImage" src={videoCardImage} alt="" />
           </div>
